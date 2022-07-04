6720aae8d0f929b6f34b2360840da33493d78569
liuyang19900520 <liuyang19900520@hotmail.com>
2022-07-04 22:35:32 +0900

    Initial Commit

---
+++ LanguageX/LanguageXApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageXApp.swift
+//  LanguageX
+//
+//  Created by LiJiao on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct LanguageXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
